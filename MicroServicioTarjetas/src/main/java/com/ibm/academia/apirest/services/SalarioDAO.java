dc8e54aef4cf294200ab63b80e710ea0ea649dca
Omar Eduardo <51302414+omareduardo150412@users.noreply.github.com>
2021-12-11 13:40:56 -0600

    Delete MicroServicioTarjetas directory

--- MicroServicioTarjetas/src/main/java/com/ibm/academia/apirest/services/SalarioDAO.java
+++
@@ -1,7 +0,0 @@
-package com.ibm.academia.apirest.services;
-import com.ibm.academia.apirest.entities.Salario;
-
-public interface SalarioDAO extends GenericoDAO<Salario>
-{
-	public Iterable<Salario> findByPasionId(Integer pasionId);
-}
--- MicroServicioTarjetas/src/main/java/com/ibm/academia/apirest/services/TarjetaDAO.java
+++
@@ -1,8 +0,0 @@
-package com.ibm.academia.apirest.services;
-
-import com.ibm.academia.apirest.entities.Tarjeta;
-
-public interface TarjetaDAO extends GenericoDAO<Tarjeta>
-{
-	public Iterable<Tarjeta>findByEdadesId(Integer edadId);
-}
